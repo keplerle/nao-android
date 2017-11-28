c4be266cae0531f58469c67ccf4c1f3420e63bc2
kepler.le <kepler.le@free.fr>
2017-11-28 11:53:49 +0100

    update

--- app/src/main/java/com/example/kinma/test_call_ws/model/Eleve.java
+++ app/src/main/java/com/example/kinma/test_call_ws/model/Eleve.java
@@ -5,4 +5,53 @@ package com.example.kinma.test_call_ws.model;
  */
 
 public class Eleve {
+    private Long id;
+    private String nom;
+    private String prenom;
+    private int bonne_reponse;
+    private int fausse_reponse;
+
+    public Eleve() {
+        super();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public int getBonne_reponse() {
+        return bonne_reponse;
+    }
+
+    public void setBonne_reponse(int bonne_reponse) {
+        this.bonne_reponse = bonne_reponse;
+    }
+
+    public int getFausse_reponse() {
+        return fausse_reponse;
+    }
+
+    public void setFausse_reponse(int fausse_reponse) {
+        this.fausse_reponse = fausse_reponse;
+    }
 }
--- app/src/main/java/com/example/kinma/test_call_ws/model/NAO.java
+++ app/src/main/java/com/example/kinma/test_call_ws/model/NAO.java
@@ -5,4 +5,80 @@ package com.example.kinma.test_call_ws.model;
  */
 
 public class NAO {
+    private Long id;
+    private int temperature_moteur;
+    private int temperature_hardware;
+    private int batterie;
+    private boolean plus;
+    private boolean moins;
+    private boolean multiple;
+    private boolean divise;
+
+    public NAO() {
+        super();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getTemperature_moteur() {
+        return temperature_moteur;
+    }
+
+    public void setTemperature_moteur(int temperature_moteur) {
+        this.temperature_moteur = temperature_moteur;
+    }
+
+    public int getTemperature_hardware() {
+        return temperature_hardware;
+    }
+
+    public void setTemperature_hardware(int temperature_hardware) {
+        this.temperature_hardware = temperature_hardware;
+    }
+
+    public int getBatterie() {
+        return batterie;
+    }
+
+    public void setBatterie(int batterie) {
+        this.batterie = batterie;
+    }
+
+    public boolean isPlus() {
+        return plus;
+    }
+
+    public void setPlus(boolean plus) {
+        this.plus = plus;
+    }
+
+    public boolean isMoins() {
+        return moins;
+    }
+
+    public void setMoins(boolean moins) {
+        this.moins = moins;
+    }
+
+    public boolean isMultiple() {
+        return multiple;
+    }
+
+    public void setMultiple(boolean multiple) {
+        this.multiple = multiple;
+    }
+
+    public boolean isDivise() {
+        return divise;
+    }
+
+    public void setDivise(boolean divise) {
+        this.divise = divise;
+    }
 }
